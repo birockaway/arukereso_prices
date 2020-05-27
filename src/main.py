79f77684bc5d5710e4355eb2de10df226949a746
hrubanj <jiri.hruban@email.cz>
2020-05-27 16:31:39 +0200

    Functioning version with consts

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import csv
 import re
 import os
+import sys
 from contextlib import contextmanager
 from io import StringIO
 import logging
@@ -11,21 +12,23 @@ from keboola import docker
 
 
 @contextmanager
-def sftp_connection(server, port, username, password, rsa_key, passphrase):
+def sftp_connection(server_address, port_number, username, password_con, rsa, passphrase_key):
     logger.info('Establishing sftp connection.')
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    pkey = paramiko.RSAKey.from_private_key(StringIO(rsa_key), password=passphrase)
+    pkey = paramiko.RSAKey.from_private_key(StringIO(rsa), password=passphrase_key)
+    sftp_con = None
     try:
-        ssh.connect(server, port=port, username=username,
-                    password=password, pkey=pkey)
-        sftp = ssh.open_sftp()
-        yield sftp
-    except Exception as e:
-        logger.error(f'Failed to establish SFTP connection. Exception {e}')
+        ssh.connect(server_address, port=port_number, username=username,
+                    password=password_con, pkey=pkey)
+        sftp_con = ssh.open_sftp()
+        yield sftp_con
+    except Exception as exc:
+        logger.error(f'Failed to establish SFTP connection. Exception {exc}')
     finally:
-        logger.info('Closing sftp.')
-        sftp.close()
+        if sftp_con is not None:
+            logger.info('Closing sftp.')
+            sftp_con.close()
         logger.info('Closing ssh.')
         ssh.close()
 
@@ -34,7 +37,7 @@ def process_line(line, **kwargs):
     processed_eshops = []
     results = []
     # the order is important
-    # if highlighted, we want to preserve the info
+    # if highlighted, we want to preserve the info adn ignore other records for the same shop
     for mapping in HIGHLIGHTED_FIELDS + OBSERVED_FIELDS + CHEAPEST_FIELDS + MALL_FIELDS:
         full_mapping = {**COMMON_FIELDS, **mapping}
         shop_data = {
@@ -44,8 +47,7 @@ def process_line(line, **kwargs):
         if mapping == MALL_FIELDS[0]:
             shop_data['ESHOP'] = 'mall.hu'
             shop_data['AVAILABILITY'] = ''
-        if shop_data['ESHOP'] != '' and shop_data[
-            'ESHOP'] not in processed_eshops:
+        if shop_data['ESHOP'] != '' and shop_data['ESHOP'] not in processed_eshops:
             if 'Highlighted' in list(mapping.keys())[0]:
                 shop_data['HIGHLIGHTED_POSITION'] = re.findall(
                     r'\d+',
@@ -61,112 +63,129 @@ def process_line(line, **kwargs):
 
 
 def get_file_dicts(filepath):
-    with open(filepath, 'r') as f:
-        timestamp = f.readline().strip('\n')
-        reader = csv.DictReader(f, delimiter=';')
+    name = filepath.split('/')[-1]
+    with open(filepath, 'r') as fl:
+        # data for shops start at the second line
+        timestamp = fl.readline().strip('\n')
+        reader = csv.DictReader(fl, delimiter=';')
         for line in reader:
             line_dicts = process_line(line,
                                       file_timestamp=timestamp,
                                       filename=name)
             yield line_dicts
 
+if __name__ == '__main__':
+    logger = logging.getLogger()
+    handler = logging.StreamHandler()
+    formatter = LogstashFormatterV1()
 
-logger = logging.getLogger()
-handler = logging.StreamHandler()
-formatter = LogstashFormatterV1()
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(level='DEBUG')
 
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-logger.setLevel(level='DEBUG')
+    datadir = os.getenv('KBC_DATADIR', '/data/')
+    cfg = docker.Config(datadir)
+    parameters = cfg.get_parameters()
 
-datadir = os.getenv('KBC_DATADIR')
-cfg = docker.Config(datadir)
-parameters = cfg.get_parameters()
+    # log parameters (excluding sensitive designated by '#')
+    logger.info({k: v for k, v in parameters.items() if "#" not in k})
 
-# log parameters (excluding sensitive designated by '#')
-logger.info({k: v for k, v in parameters.items() if "#" not in k})
+    previous_timestamp_filename = parameters.get('previous_timestamp_filename')
+    filename_pattern = parameters.get('filename_pattern')
+    server = parameters.get('server')
+    port = int(parameters.get('port'))
+    user = parameters.get('username')
+    password = parameters.get('#password')
+    passphrase = parameters.get('#passphrase')
+    rsa_key = parameters.get('#key')
+    wanted_columns = parameters.get('wanted_columns')
 
-previous_timestamp_filename = parameters.get('previous_timestamp_filename')
-filename_pattern = parameters.get('filename_pattern')
-server = parameters.get('server')
-port = int(parameters.get('port'))
-username = parameters.get('username')
-password = parameters.get('#password')
-passphrase = parameters.get('#passphrase')
-rsa_key = parameters.get('#key')
-wanted_columns = parameters.get('wanted_columns')
+    SFTP_FOLDER = '/upload/'
 
-SFTP_FOLDER = '/upload/'
+    COMMON_FIELDS = {
+        'ItemCode': 'MATERIAL',
+        'EAN': 'EAN',
+        'AKIdentifier': 'CSE_ID',
+        'AKCategoryName': 'CATEGORY_NAME',
+        'Rating': 'RATING',
+        'ReviewCount': 'REVIEW_COUNT'
+    }
 
-COMMON_FIELDS = {
-    'ItemCode': 'MATERIAL',
-    'EAN': 'EAN',
-    'AKIdentifier': 'CSE_ID',
-    'AKCategoryName': 'CATEGORY_NAME',
-    'Rating': 'RATING',
-    'ReviewCount': 'REVIEW_COUNT'
-}
+    HIGHLIGHTED_FIELDS = [{
+        f'Highlighted{i} EshopName': 'ESHOP',
+        f'Highlighted{i} Price': 'PRICE',
+        f'Highlighted{i} Stock': 'AVAILABILITY',
+        f'Highlighted{i} ShippingPrice': 'SHIPPING_PRICE'
+    } for i in range(1, 4)]
 
-HIGHLIGHTED_FIELDS = [{
-    f'Highlighted{i} EshopName': 'ESHOP',
-    f'Highlighted{i} Price': 'PRICE',
-    f'Highlighted{i} Stock': 'AVAILABILITY',
-    f'Highlighted{i} ShippingPrice': 'SHIPPING_PRICE'
-} for i in range(1, 4)]
+    OBSERVED_FIELDS = [{
+        f'Observed{i} Name': 'ESHOP',
+        f'Observed{i} Price': 'PRICE',
+        f'Observed{i} Stock': 'AVAILABILITY',
+        f'Observed{i} ShippingPrice': 'SHIPPING_PRICE'
+    } for i in range(1, 6)]
 
-OBSERVED_FIELDS = [{
-    f'Observed{i} Name': 'ESHOP',
-    f'Observed{i} Price': 'PRICE',
-    f'Observed{i} Stock': 'AVAILABILITY',
-    f'Observed{i} ShippingPrice': 'SHIPPING_PRICE'
-} for i in range(1, 6)]
+    CHEAPEST_FIELDS = [{
+        'Cheapest EshopName': 'ESHOP',
+        'Cheapest Price': 'PRICE',
+        'Cheapest Stock': 'AVAILABILITY',
+        'Cheapest ShippingPrice': 'SHIPPING_PRICE'
+    }]
 
-CHEAPEST_FIELDS = [{
-    'Cheapest EshopName': 'ESHOP',
-    'Cheapest Price': 'PRICE',
-    'Cheapest Stock': 'AVAILABILITY',
-    'Cheapest ShippingPrice': 'SHIPPING_PRICE'
-}]
+    MALL_FIELDS = [{'Price': 'PRICE', 'Position': 'POSITION'}]
 
-MALL_FIELDS = [{'Price': 'PRICE', 'Position': 'POSITION'}]
+    CONSTANT_FIELDS = {'COUNTRY': 'HU', 'DISTRCHAN': 'MA', 'SOURCE': 'arukereso', 'FREQ': 'd'}
 
-CONSTANT_FIELDS = {'COUNTRY': 'HU', 'DISTRCHAN': 'MA', 'SOURCE': 'arukereso', 'FREQ': 'd'}
+    with open(f'{datadir}in/tables/{previous_timestamp_filename}') as input_file:
+        previous_timestamp_list = [
+            str(ts.replace('"', ''))
+            for ts
+            # read all input file rows, except the header
+            in input_file.read().split(os.linesep)[1:]
+        ]
+        previous_timestamp = float(previous_timestamp_list[0])
 
-with open(f'{datadir}in/tables/{previous_timestamp_filename}') as input_file:
-    previous_timestamp_list = [
-        str(ts.replace('"', ''))
-        for ts
-        # read all input file rows, except the header
-        in input_file.read().split(os.linesep)[1:]
-    ]
-    previous_timestamp = float(previous_timestamp_list[0])
+    last_timestamp = previous_timestamp
+    files_to_process = []
 
-last_timestamp = previous_timestamp
-files_to_process = []
+    destroot = f'{datadir}in/tables/downloaded_csvs'
+    if not os.path.exists(destroot):
+        os.makedirs(destroot)
 
-destroot = f'{datadir}in/tables/downloaded_csvs'
-if not os.path.exists(destroot):
-    os.makedirs(destroot)
-# NB: original script downloaded both from upload and upload/archive
-# archive seems to contain only records that are several days old
-folder = SFTP_FOLDER
-with sftp_connection(server, port, username, password, rsa_key, passphrase) as sftp:
-    for file in sftp.listdir_attr(folder):
-        modified_time = file.st_mtime
-        if (modified_time > previous_timestamp) and file.filename.startswith(filename_pattern):
-            if modified_time > last_timestamp:
-                last_timestamp = modified_time
-            filepath = f'{folder}{file.filename}'
-            logger.info(f'Downloading file {filepath}')
-            destpath = f'{destroot}/{file.filename}'
-            files_to_process.append(destpath)
-            sftp.get(filepath, destpath)
+    # NB: original script downloaded both from upload and upload/archive
+    # archive seems to contain only records that are several days old
+    folder = SFTP_FOLDER
+    with sftp_connection(server, port, user, password, rsa_key, passphrase) as sftp:
+        for file in sftp.listdir_attr(folder):
+            modified_time = file.st_mtime
+            if (modified_time > previous_timestamp) and file.filename.startswith(filename_pattern):
+                if modified_time > last_timestamp:
+                    last_timestamp = modified_time
+                sourcepath = f'{folder}{file.filename}'
+                logger.info(f'Downloading file {sourcepath}')
+                destpath = f'{destroot}/{file.filename}'
+                files_to_process.append(destpath)
+                sftp.get(sourcepath, destpath)
 
-logger.info(f'Downloaded {len(files_to_process)} files.')
+    if not files_to_process:
+        logger.info('No new files to process. Exiting.')
+        sys.exit(0)
 
-with open(f'{datadir}out/tables/results.csv', 'w+') as f:
-    writer = csv.DictWriter(f, fieldnames=wanted_columns)
-    for file in files_to_process:
-        logger.info(f'Processing file: {file}')
-        for result_dicts in get_file_dicts(file):
-            writer.writerows(result_dicts)
+    logger.info(f'Downloaded {len(files_to_process)} files.')
+
+    with open(f'{datadir}out/tables/results.csv', 'w+') as f:
+        writer = csv.DictWriter(f, fieldnames=wanted_columns, extrasaction='ignore')
+        writer.writeheader()
+        for file in files_to_process:
+            logger.info(f'Processing file: {file}')
+            try:
+                for result_dicts in get_file_dicts(file):
+                    writer.writerows(result_dicts)
+            except Exception as e:
+                logger.error(f'Failed to proces file: {file}. Exception {e}.')
+
+    logger.info('Processing done. Writing last timestamp.')
+    with open(f'{datadir}out/tables/arukereso_last_timestamp.csv', 'w+') as fo:
+        dict_writer = csv.DictWriter(fo, fieldnames=['max_timestamp_this_run'])
+        dict_writer.writeheader()
+        dict_writer.writerow([last_timestamp])
